4eddf1a05a5371727b4c323bd828afab262cb92c
Juozas L <lizdenis.juozas@gmail.com>
2018-05-24 14:22:36 +0100

    lemp18 fix

--- lemp18/run.sh
+++ lemp18/run.sh
@@ -4,7 +4,7 @@ set -e
 
 NGINX_REALIP_PROXY=${NGINX_REALIP_PROXY:-"172.17.0.1"}
 FRAMEWORK=${FRAMEWORK:-"yii2"}
-WEB_DIR=${WEB_DIR:-"/var/www"}
+WEB_DIR=${WEB_DIR:-"/var/www/app"}
 
 # ++ Mysql
 
@@ -24,6 +24,7 @@ fi
 # ++ nginx & php-fpm
 
 mkdir -p /var/www/log/
+mkdir -p "$WEB_DIR"
 chown -R www-data:www-data "$WEB_DIR"
 
 service php7.2-fpm start
@@ -44,30 +45,31 @@ fi
 
 if [ "$FRAMEWORK" == "yii2" ]; then
 
-    mkdir -p /var/www/app/web/
+    mkdir -p "$WEB_DIR"/web/
 
     cp /etc/nginx/sites-available/default_yii2 /etc/nginx/sites-available/default
 
     # index file initialization if not exist
-    if [ ! -f /var/www/app/web/index.php ]; then
-        echo "<?php echo 'container ...'; ?>" > /var/www/app/web/index.php
+    if [ ! -f "$WEB_DIR"/web/index.php ]; then
+        echo "<?php echo 'container ...'; ?>" > "$WEB_DIR"/web/index.php
     fi
 
 fi
 
 if [ "$FRAMEWORK" == "laravel" ]; then
 
-    mkdir -p /var/www/app/public/
+    mkdir -p "$WEB_DIR"/public/
 
     cp /etc/nginx/sites-available/default_laravel /etc/nginx/sites-available/default
 
     # index file initialization if not exist
     if [ ! -f /var/www/app/public/index.php ]; then
-        echo "<?php echo 'container ...'; ?>" > /var/www/app/public/index.php
+        echo "<?php echo 'container ...'; ?>" > "$WEB_DIR"/public/index.php
     fi
 
 fi
 
+sed -i 's/\/var\/www\/app/'${WEB_DIR//\//\\\/}'/' /etc/nginx/sites-available/default
 sed -i 's/set_real_ip_from 0.0.0.0;/set_real_ip_from '$NGINX_REALIP_PROXY';/' /etc/nginx/sites-available/default
 
 # super visor deamons start
