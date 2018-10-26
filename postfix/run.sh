b353cd6ca8134b0921d4827c130d234207815b8c
Juozas Lizdenis <juozas@blackfordanalysis.com>
2018-10-26 07:52:20 +0100

    # postfix - removed crontab file from run/sh

--- postfix/run.sh
+++ postfix/run.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-# cron file initialize if not exist
-if [ ! -f /etc/cron.d/app ]; then
-    touch /etc/cron.d/app
-    chmod 0644 /etc/cron.d/app
-fi
-
 # super visor deamons start
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
