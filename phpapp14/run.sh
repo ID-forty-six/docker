98b4eec5ffa449a67d073f03154bda687de3e1d2
Tautvydas Bargelis <ademas@gmail.com>
2018-12-22 19:56:14 +0200

    pridetas plain php support, php konfigai

--- phpapp14/run.sh
+++ phpapp14/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 NGINX_REALIP_PROXY=${NGINX_REALIP_PROXY:-"172.17.0.1"}
-FRAMEWORK=${FRAMEWORK:-"yii2"}
+FRAMEWORK=${FRAMEWORK:-"plain"}
 
 
 # ++ nginx & php-fpm
@@ -27,6 +27,17 @@ fi
 
 # index file initialization if not exist
 
+if [ "$FRAMEWORK" == "plain" ]; then
+
+    cp /etc/nginx/sites-available/default_yii2 /etc/nginx/sites-available/default
+
+    # index file initialization if not exist
+    if [ ! -f /var/www/app/index.php ]; then
+        echo "<?php echo 'container plain'; ?>" > /var/www/app/index.php
+    fi
+
+fi
+
 if [ "$FRAMEWORK" == "yii2" ]; then
 
     mkdir -p /var/www/app/web/
@@ -35,7 +46,7 @@ if [ "$FRAMEWORK" == "yii2" ]; then
 
     # index file initialization if not exist
     if [ ! -f /var/www/app/web/index.php ]; then
-        echo "<?php echo 'container ...'; ?>" > /var/www/app/web/index.php
+        echo "<?php echo 'container yii2'; ?>" > /var/www/app/web/index.php
     fi
 
 fi
@@ -48,12 +59,14 @@ if [ "$FRAMEWORK" == "laravel" ]; then
 
     # index file initialization if not exist
     if [ ! -f /var/www/app/public/index.php ]; then
-        echo "<?php echo 'container ...'; ?>" > /var/www/app/public/index.php
+        echo "<?php echo 'container laravel'; ?>" > /var/www/app/public/index.php
     fi
 
 fi
 
 sed -i 's/set_real_ip_from 0.0.0.0;/set_real_ip_from '$NGINX_REALIP_PROXY';/' /etc/nginx/sites-available/default
+
+# paruosiame default failus
 
 # super visor deamons start
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
